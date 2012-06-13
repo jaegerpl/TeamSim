31b0346f2bbf04fc1596dbe2de1f179f3cf7b54f
Pascal Jäger <pascal@pascaljaeger.de>
2012-06-13 07:53:58 +0200

    lib configuration

---
+++ lib/apache-jena-2-1.7.0-incubating/bat/utf8.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if NOT "%JENAROOT%" == "" goto :okRoot
+echo JENAROOT not set
+exit /B
+
+:okRoot
+
+set JVM_ARGS=-Xmx1024M
+set JENA_CP="%JENAROOT%\lib\*;"
+set LOGGING=-Dlog4j.configuration=file:%JENAROOT%/log4j.properties
+
+java %JVM_ARGS% %LOGGING% -cp %JENA_CP% riotcmd.utf8 %*
+exit /B
